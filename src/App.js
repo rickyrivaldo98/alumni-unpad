8a2797b47efc3eeb84ba9c4648242d0fc1fcc151
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-04-19 22:18:08 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link, Switch, Route } from "react-router-dom";
 import Landing from "./Page/Landing";
+import Footer from "./Page/layout/Footer";
+import Navbar from "./Page/layout/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route path="/">
           <Landing />
         </Route>
       </Switch>
+      <Footer />
     </>
   );
 };
