17d3d6ad8d7ca67eae9fa0e6c18e0515ee21828a
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-04-19 14:00:57 +0700

    nambah berita

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link, Switch, Route } from "react-router-dom";
+import DetailBerita from "./Page/DetailBerita";
+import GalleryPhoto from "./Page/Gallery";
 import Landing from "./Page/Landing";
 
 const App = () => {
   return (
     <>
       <Switch>
+        <Route path="/gallery">
+          <GalleryPhoto />
+        </Route>
+        <Route path="/detail-berita">
+          <DetailBerita />
+        </Route>
         <Route path="/">
           <Landing />
         </Route>
---
+++ src/Page/DetailBerita.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const DetailBerita = () => {
+  return (
+    <>
+      <div
+        className="bg-detailBerita  "
+        // menampilkan gambar ketika di klik
+        style={{
+          backgroundImage:
+            "url(https://source.unsplash.com/5P91SF0zNsI/740x494)",
+        }}
+      >
+        <div className="container p-3 mx-auto ">
+          <div className=" xl:mt-32 lg:mt-24 ml-32  ">
+            <div className="">
+              IKA Unpad Gelar Sosialisasi dan Pelatihan Terkait UU Cipta Kerja
+              IKA Unpad Gelar Sosialisasi dan Pelatihan Terkait UU Cipta Kerja
+              IKA Unpad Gelar Sosialisasi dan Pelatihan Terkait UU Cipta Kerja
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailBerita;
