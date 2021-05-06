0027c5a8a190527554fe338c0d8d11b6a0af71e6
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2021-05-06 22:37:26 +0700

    Add form category admin

---
+++ src/Page/components/Admin/AdminCategory.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DataCategory from "./DataCategory";
+
+const AdminBerita = () => {
+  return (
+    <>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4">
+          <DataCategory />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminBerita;
