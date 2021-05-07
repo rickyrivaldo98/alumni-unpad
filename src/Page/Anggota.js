28efc1496d4c6c7321d51e40317ffad110cf184b
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-05-07 19:17:45 +0700

    filter search

--- src/Page/Anggota.js
+++ src/Page/Anggota.js
@@ -13,6 +13,8 @@ const Anggota = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [empty, setEmpty] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filterData, setFilterData] = useState([]);
 
   useEffect(() => {
     setLoading(true);
@@ -27,6 +29,14 @@ const Anggota = () => {
       });
     setLoading(false);
   }, [data]);
+
+  useEffect(() => {
+    setFilterData(
+      data.filter((searchdata) =>
+        searchdata.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, data]);
 
   let i = 1;
 
@@ -50,20 +60,7 @@ const Anggota = () => {
             <div className="my-2 flex sm:flex-row flex-col">
               <div className="flex flex-row mb-1 sm:mb-0">
                 <div className="relative">
-                  <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                    <option>5</option>
-                    <option>10</option>
-                    <option>20</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg
-                      className="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"></div>
                 </div>
                 {/* <div className="relative">
                   <select className="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
@@ -94,6 +91,7 @@ const Anggota = () => {
                 <input
                   placeholder="Search"
                   className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+                  onChange={(e) => setSearch(e.target.value)}
                 />
               </div>
             </div>
@@ -141,7 +139,7 @@ const Anggota = () => {
                           </div>
                         )}
                         {!loading &&
-                          data.map((x) => (
+                          filterData.map((x) => (
                             <tr>
                               <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <p className="text-gray-900 whitespace-no-wrap">
