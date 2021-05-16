4281c4a199276f0d85384d9a22858da2ba9582e1
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-05-16 19:46:58 +0700

    edit

--- src/Page/components/Admin/Forms/EditBerita.js
+++ src/Page/components/Admin/Forms/EditBerita.js
@@ -5,15 +5,24 @@ import { useParams } from "react-router-dom";
 import FormData from "form-data";
 import { data } from "autoprefixer";
 import { useAlert } from "react-alert";
-import { EditorState, convertToRaw, ContentState } from "draft-js";
+import {
+  EditorState,
+  convertToRaw,
+  convertFromRaw,
+  ContentState,
+  convertFromHTML,
+  CompositeDecorator,
+  ContentBlock,
+} from "draft-js";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { convertToHTML, convertFromHTML } from "draft-convert";
+import { convertToHTML /*convertFromHTML */ } from "draft-convert";
 import { Editor } from "react-draft-wysiwyg";
 // import DOMPurify from "dompurify";
 import slugify from "react-slugify";
 import htmlToDraft from "html-to-draftjs";
+import { stateFromHTML } from "draft-js-import-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 // import { FaWindows } from "react-icons/fa";
@@ -28,7 +37,40 @@ const EditBerita = () => {
   //     __html: DOMPurify.sanitize(html),
   //   };
   // };
+  const [loading, setLoading] = useState();
 
+  const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [oldContent, setOldContent] = useState("");
+
+  const [Title, setTitle] = useState("");
+  const [Image, setImage] = useState("");
+  const [Category, setCategory] = useState("");
+  useEffect(() => {
+    axios
+      .get(`https://unpad.sarafdesign.com/berita/id/${id}`)
+      .then((res) => {
+        setLoading(true);
+        setContent(
+          EditorState.createWithContent(
+            ContentState.createFromBlockArray(
+              convertFromHTML(res.data[0].content)
+            )
+          )
+        );
+        setData2(res.data[0]);
+        setTitle(res.data[0].title);
+        // testhtml2 = res.data[0].content;
+        // setOldContent(htmlToDraft(testhtml2));
+        setCategory(res.data[0].category_id);
+        setImage(res.data[0].thumbnail);
+        // console.log(testhtml2);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setData([]);
+      });
+  }, []);
   useEffect(() => {
     axios
       .get(`https://unpad.sarafdesign.com/category`)
@@ -40,42 +82,53 @@ const EditBerita = () => {
       });
   }, []);
 
-  useEffect(() => {
-    axios
-      .get(`https://unpad.sarafdesign.com/berita/id/${id}`)
-      .then((res) => {
-        setData2(res.data[0]);
-        setTitle(res.data[0].title);
-        setOldContent(res.data[0].content);
+  console.log(data2);
 
-        setCategory(res.data[0].category_id);
-        setImage(res.data[0].thumbnail);
-        console.log(res.data[0]);
-      })
-      .catch((error) => {
-        setData([]);
-      });
-  }, []);
-  const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
-
-  const [Title, setTitle] = useState("");
-  const [Image, setImage] = useState("");
-  const [Category, setCategory] = useState("");
-  const [oldContent, setOldContent] = useState("");
   // const converthtml = EditorState.createWithContent(
   //   convertFromHTML(oldContent)
   // );
-  const testhtml = "<p>Hey this <strong>editor</strong> rocks 😀</p>";
-  const testhtml2 = `${oldContent}`;
-  const converthtml = htmlToDraft(testhtml);
-  const contentState = ContentState.createFromBlockArray(
-    converthtml.contentBlocks
-  );
+  // const testhtml = `<p>Haloo ini test</p> `;
+  // const testhtml2 = `${oldContent}`;
+  // console.log("ini oldContent2 " + testhtml2);
+
+  // const testhtml2 = `${oldContent}`;
+  // const converthtml = stateFromHTML(testhtml);
+  // const converthtml2 = stateFromHTML(oldContent);
+
+  // const converthtml = testhtml;
+  // const converthtml2 = oldContent;
+
+  // const converthtml = oldContent;
+  let editorStateInitial;
+  // if (converthtml) {
+  //   // const contentState = ContentState.createFromBlockArray(
+  //   //   converthtml.contentBlocks,
+  //   //   converthtml.entityMap
+  //   // );
+  //   // console.log(`data testhtml ${converthtml}`);
+  //   // console.log(`data testhtml2 ${converthtml2}`);
+  //   editorStateInitial = EditorState.createWithContent(converthtml2);
+  // } else {
+  //   // const contentState2 = ContentState.createFromBlockArray(
+  //   //   converthtml2.contentBlocks,
+  //   //   converthtml2.entityMap
+  //   // );
+  //   // editorStateInitial = EditorState.createWithContent(contentState2);
+  //   editorStateInitial = EditorState.createEmpty();
+  // }
   // const isikonten = EditorState.createWithContent(contentState);
-  const [Content, setContent] = useState(
-    EditorState.createWithContent(contentState)
-  );
+
+  // if (!loading) {
+  //   editorStateInitial = EditorState.createWithContent(
+  //     ContentState.createFromBlockArray(convertFromHTML(testhtml))
+  //   );
+  // }
+  const [Content, setContent] = useState(EditorState.createEmpty());
+  // EditorState.createWithContent(
+  //   ContentState.createFromBlockArray(convertFromHTML(testhtml))
+  // )
+  // editorStateInitial
+
   // const [convertedHtml, setConvertedHtml] = useState(() =>
   //   EditorState.createWithContent(oldContent)
   // );
@@ -95,8 +148,10 @@ const EditBerita = () => {
     setConvertedContent(currentContentAsHTML);
   };
   const handleImage = (e) => setImage(e.target.files[0]);
-  console.log("ini testhtml " + testhtml);
-  console.log("ini testhtml2" + testhtml2);
+  // console.log("ini testhtml " + testhtml);
+  // console.log("ini converted " + convertedContent);
+
+  // console.log("ini testhtml2 " + testhtml2);
 
   let editBerita = (e) => {
     e.preventDefault();
@@ -107,29 +162,33 @@ const EditBerita = () => {
     // berita.set("content", convertedContent);
     // berita.set("file", Image);
     if (Category === "") {
-      berita.set("category_id", data.category_id);
+      berita.set("category_id", data2.category_id);
     } else {
       berita.set("category_id", Category);
     }
     if (Title === "") {
-      berita.set("title", data.title);
+      berita.set("title", data2.title);
     } else {
       berita.set("title", Title);
     }
     if (Title === "") {
-      berita.set("slug_title", slugify(data.title));
+      berita.set("slug_title", slugify(data2.title));
     } else {
       berita.set("slug_title", slugify(Title));
     }
     if (Content === "") {
-      berita.set("content", data.content);
+      berita.set("content", data2.content);
     } else {
       berita.set("content", convertedContent);
     }
     if (Image === "") {
-      berita.set("file", data.thumbnail);
+      berita.set("file", data2.thumbnail);
     } else {
       berita.set("file", Image);
+    }
+
+    for (var pair of berita.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
     }
     const config = {
       headers: {
@@ -140,7 +199,7 @@ const EditBerita = () => {
     };
     axios
       .put(
-        `https://unpad.sarafdesign.com/berita/${data.id}/${data.thumbnail}`,
+        `https://unpad.sarafdesign.com/berita/${data2.id}/${data2.thumbnail}`,
         berita,
         config
       )
@@ -165,133 +224,140 @@ const EditBerita = () => {
   // });
   // console.log(Category);
 
-  let html = "";
   return (
     <>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-10/12 mb-12 px-4">
-          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-            <div className="rounded-t bg-white mb-0 px-6 py-6">
-              <div className="text-center flex justify-start">
-                <button
-                  className="bg-blue-500 text-white active:bg-lightBlue-600 font-bold  text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => window.history.back()}
-                >
-                  Back
-                </button>
-                <h6 className="m-2 text-blueGray-700 text-xl font-bold">
-                  Edit Berita
-                </h6>
-              </div>
-            </div>
-            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-              <form onSubmit={(e) => editBerita(e)}>
-                <div className="flex flex-col flex-wrap">
-                  <div className="w-full lg:w-6/12 px-4">
-                    <div className="relative w-full mb-3">
-                      <label
-                        className="block   text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="grid-password"
-                      >
-                        Category
-                      </label>
-                      <select
-                        name="Category"
-                        value={Category}
-                        onChange={(e) => {
-                          handleCategory(e);
-                        }}
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                        id="Category"
-                        // {...register("Category", { required: true })}
-                      >
-                        <option value={Category} selected>
-                          {Category}
-                        </option>
+      {loading ? (
+        <>
+          <div>loading</div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-wrap mt-4">
+            <div className="w-10/12 mb-12 px-4">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+                <div className="rounded-t bg-white mb-0 px-6 py-6">
+                  <div className="text-center flex justify-start">
+                    <button
+                      className="bg-blue-500 text-white active:bg-lightBlue-600 font-bold  text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => window.history.back()}
+                    >
+                      Back
+                    </button>
+                    <h6 className="m-2 text-blueGray-700 text-xl font-bold">
+                      Edit Berita
+                    </h6>
+                  </div>
+                </div>
+                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <form onSubmit={(e) => editBerita(e)}>
+                    <div className="flex flex-col flex-wrap">
+                      <div className="w-full lg:w-6/12 px-4">
+                        <div className="relative w-full mb-3">
+                          <label
+                            className="block   text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Category
+                          </label>
+                          <select
+                            name="Category"
+                            value={Category}
+                            onChange={(e) => {
+                              handleCategory(e);
+                            }}
+                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                            id="Category"
+                            // {...register("Category", { required: true })}
+                          >
+                            <option value={Category} selected>
+                              {Category}
+                            </option>
 
-                        {data.map((x) => (
-                          <option value={x.id}>{x.name}</option>
-                        ))}
-                      </select>
-                    </div>
-                    <div className="relative w-full mb-3">
-                      <label
-                        className="block   text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="grid-password"
-                      >
-                        Title
-                      </label>
-                      <input
-                        type="text"
-                        name="title"
-                        placeholder="Insert Title"
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                        value={Title}
-                        onChange={(e) => {
-                          handleTitle(e);
-                        }}
-                        // {...register("title", { required: true })}
-                      />
-                      {/* <p style={{ color: "red" }}>
+                            {data.map((x) => (
+                              <option value={x.id}>{x.name}</option>
+                            ))}
+                          </select>
+                        </div>
+                        <div className="relative w-full mb-3">
+                          <label
+                            className="block   text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Title
+                          </label>
+                          <input
+                            type="text"
+                            name="title"
+                            placeholder="Insert Title"
+                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                            value={Title}
+                            onChange={(e) => {
+                              handleTitle(e);
+                            }}
+                            // {...register("title", { required: true })}
+                          />
+                          {/* <p style={{ color: "red" }}>
                         {errors.Title?.message}
                       </p> */}
-                    </div>
-                    <div className="relative w-full mb-3">
-                      <label
-                        className="block text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="grid-password"
-                      >
-                        Content
-                      </label>
-                      {/* <textarea
+                        </div>
+                        <div className="relative w-full mb-3">
+                          <label
+                            className="block text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Content
+                          </label>
+                          {/* <textarea
                         disabled
                         value={draftToHtml(
                           convertToRaw(editorState.getCurrentContent())
                         )}
                       /> */}
-                      <Editor
-                        editorState={Content}
-                        // onChange={setContent}
-                        // initialContentState={Content}
-                        toolbarClassName="toolbarClassName"
-                        wrapperClassName="wrapperClassName"
-                        editorClassName="editorClassName"
-                        // onContentStateChange={setContent}
-                        onEditorStateChange={handleContent}
-                      />
+                          <Editor
+                            editorState={Content}
+                            // onChange={setContent}
+                            // initialContentState={Content}
+                            toolbarClassName="toolbarClassName"
+                            wrapperClassName="wrapperClassName"
+                            editorClassName="editorClassName"
+                            // onContentStateChange={setContent}
+                            onEditorStateChange={handleContent}
+                          />
+                        </div>
+                        <div className="relative w-full mb-3">
+                          <label
+                            className="block  text-blueGray-600 text-xs font-bold mb-2"
+                            htmlFor="grid-password"
+                          >
+                            Image
+                          </label>
+                          <img
+                            src={`https://unpad.sarafdesign.com/uploads/${Image}`}
+                            alt=""
+                          />
+                          <input
+                            onChange={handleImage}
+                            type="file"
+                            placeholder="input file image"
+                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                          />
+                        </div>
+                        <button
+                          className="bg-green-500 text-white active:bg-lightBlue-600 font-bold  text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                          type="submit"
+                        >
+                          Save
+                        </button>
+                      </div>
                     </div>
-                    <div className="relative w-full mb-3">
-                      <label
-                        className="block  text-blueGray-600 text-xs font-bold mb-2"
-                        htmlFor="grid-password"
-                      >
-                        Image
-                      </label>
-                      <img
-                        src={`https://unpad.sarafdesign.com/uploads/${Image}`}
-                        alt=""
-                      />
-                      <input
-                        onChange={handleImage}
-                        type="file"
-                        placeholder="input file image"
-                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      />
-                    </div>
-                    <button
-                      className="bg-green-500 text-white active:bg-lightBlue-600 font-bold  text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-                      type="submit"
-                    >
-                      Save
-                    </button>
-                  </div>
+                  </form>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
